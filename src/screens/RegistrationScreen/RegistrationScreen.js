d0dcd643cf5c38420c82983b635fa1233974d7c7
Jorge <truekarter@gmail.com>
2023-10-17 16:08:56 -0500

    Moved RegistrationScreen styles to seperate file.

--- src/screens/RegistrationScreen/RegistrationScreen.js
+++ src/screens/RegistrationScreen/RegistrationScreen.js
@@ -1,12 +1,7 @@
 import React, { useState } from 'react';
-import {
-  View,
-  Text,
-  TextInput,
-  StyleSheet,
-  TouchableOpacity,
-} from 'react-native';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import styles from './styles';
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 
 export default function RegistrationScreen({ navigation }) {
@@ -86,58 +81,3 @@ export default function RegistrationScreen({ navigation }) {
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  button: {
-    backgroundColor: 'red',
-    borderRadius: 10,
-    height: 50,
-    padding: 15,
-    marginTop: 50,
-    width: '90%',
-  },
-  buttonText: {
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-  container: {
-    alignItems: 'center',
-    backgroundColor: '#fafafc',
-    flex: 1,
-    justifyContent: 'center',
-  },
-  formContainer: {
-    backgroundColor: '#fff',
-    borderRadius: 20,
-    justifyContent: 'center',
-    paddingHorizontal: 15,
-    paddingVertical: 20,
-    width: '90%',
-  },
-  footerText: {
-    color: '#2e2e2d',
-    fontSize: 14,
-    marginTop: 10,
-  },
-  footerLink: {
-    color: 'red',
-    fontWeight: 'bold',
-  },
-  input: {
-    backgroundColor: '#fafafc',
-    borderRadius: 12,
-    height: 50,
-    marginTop: 10,
-    marginBottom: 20,
-    paddingHorizontal: 15,
-    width: '100%',
-  },
-  label: {
-    color: '#b7b6bd',
-  },
-  title: {
-    fontSize: 24,
-    marginBottom: 50,
-  },
-});
--- src/screens/RegistrationScreen/styles.js
+++ src/screens/RegistrationScreen/styles.js
@@ -1,18 +1,57 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+  button: {
+    backgroundColor: 'red',
+    borderRadius: 10,
+    height: 50,
+    padding: 15,
+    marginTop: 50,
+    width: '90%',
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
   container: {
+    alignItems: 'center',
+    backgroundColor: '#fafafc',
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+  },
+  formContainer: {
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    justifyContent: 'center',
+    paddingHorizontal: 15,
+    paddingVertical: 20,
+    width: '90%',
+  },
+  footerText: {
+    color: '#2e2e2d',
+    fontSize: 14,
+    marginTop: 10,
+  },
+  footerLink: {
+    color: 'red',
+    fontWeight: 'bold',
   },
   input: {
-    borderColor: "lightgray",
-    borderRadius: 25,
-    borderWidth: 1,
+    backgroundColor: '#fafafc',
+    borderRadius: 12,
     height: 50,
+    marginTop: 10,
+    marginBottom: 20,
     paddingHorizontal: 15,
-    width: "80%",
+    width: '100%',
+  },
+  label: {
+    color: '#b7b6bd',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 50,
   },
 });
 
